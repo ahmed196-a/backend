619de2b51499b273e303e93e3b0a14b0d86de134
ahmed196-a <ahmed457abbas@gmail.com>
2025-08-13 02:09:55 -0700

    Hello

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotene.config();
 
 const app = express();
 app.use(cors({
-    origin: "https://backend-f3m1.vercel.app",
+    origin: "https://www.mkaudiology.com/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
